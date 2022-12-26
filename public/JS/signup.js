a7da875db3feea5ee2d8ccbfd494550c0ffe0d0d
saamaamr <mamun872381cpi@gmail.com>
2022-12-27 02:00:59 +0600

    main

--- public/JS/signup.js
+++ public/JS/signup.js
@@ -11,18 +11,12 @@ if (roleValue) {
     role[0].setAttribute('selected', 'selected')
   }
 }
-
-
-
-
 const divisionsList = document.querySelector('.divisions');
 const endPoint2 = "https://bdapis.com/api/v1.1/divisions";
-
 async function bdApi(url) {
   const res = await fetch(url)
   return res.json();
 }
-
 bdApi(endPoint2)
   .then(divisions => {
     const allDivisions = divisions.data;
@@ -38,12 +32,9 @@ bdApi(endPoint2)
   .catch(error => {
     console.error('Error:', error);
   });
-
 const test = document.getElementById("division")
-
 test.onclick = function () {
   dist = document.getElementById("division").value
-
   var list = document.getElementById("district").getElementsByTagName("option");
   for (var k = list.length - 1; k >= 1; k--) {
     var item = list[k];
@@ -52,46 +43,36 @@ test.onclick = function () {
   const districtsList = document.querySelector('.districts');
   const upazilaList = document.querySelector('.upazilla');
   const endPoint = `https://bdapis.com/api/v1.1/division/${dist}`;
-
   async function bdApi(url) {
     const res = await fetch(url)
     return res.json();
   }
-
   bdApi(endPoint)
     .then(districts => {
       const allDistricts = districts.data;
-
       allDistricts.forEach(district => {
         const li = document.createElement("option");
         li.textContent = district.district;
         const att = document.createAttribute("value");
         att.value = district.district;
         districtsList.append(li);
-
       })
       document.querySelector('.districts').onclick = function () {
-
         var list = document.getElementById("upazilla").getElementsByTagName("option");
         for (var k = list.length - 1; k >= 1; k--) {
           var item = list[k];
           item.parentNode.removeChild(item);
         } 
-
         dindex = document.querySelector('.districts').value
-
         const allUpazilla = districts.data;
         allUpazilla.forEach(upazilla => {
           if (dindex == upazilla.district) {
-
             upa = upazilla.upazilla;
             for (let i = 0; i < upa.length; i++) {
               const li = document.createElement("option");
               li.textContent = upa[i];
-
               const att = document.createAttribute("value");
               att.value = upa[i];
-
               upazilaList.append(li);
             }
           }
