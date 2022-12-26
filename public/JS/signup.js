0208de8fa6fb2e0699f071fb1f5487f7a98af546
saamaamr <mamun872381cpi@gmail.com>
2022-12-27 01:31:35 +0600

    main

--- public/JS/signup.js
+++ public/JS/signup.js
@@ -3,42 +3,20 @@ const role = document.getElementById('role')
 const roleValue = localStorage.getItem('role')
 
 if (roleValue) {
-    if (roleValue == "user") {
-        role[1].setAttribute('selected', 'selected')
-    } else if (roleValue == "worker") {
-        role[2].setAttribute('selected','selected')
-    } else {
-        role[0].setAttribute('selected', 'selected')
-    }
+  if (roleValue == "user") {
+    role[1].setAttribute('selected', 'selected')
+  } else if (roleValue == "worker") {
+    role[2].setAttribute('selected', 'selected')
+  } else {
+    role[0].setAttribute('selected', 'selected')
+  }
 }
 
 
-const districtsList = document.querySelector('.districts');
-const endPoint = "https://bdapis.herokuapp.com/api/v1.1/division/barishal";
 
-async function bdApi(url) {
-  const res = await fetch(url)
-  return res.json();
-}
-
-bdApi(endPoint)
-  .then(districts => {
-    const allDistricts = districts.data;
-
-    allDistricts.forEach(district => {
-      const li = document.createElement("option");
-      li.textContent = district.district;
-      const att = document.createAttribute("value");
-      att.value = district.district;
-      districtsList.append(li);
-    })
-  })
-  .catch(error => {
-    console.error('Error::', error);
-  });
 
 const divisionsList = document.querySelector('.divisions');
-const endPoint2 = "https://bdapis.herokuapp.com/api/v1.1/divisions";
+const endPoint2 = "https://bdapis.com/api/v1.1/divisions";
 
 async function bdApi(url) {
   const res = await fetch(url)
@@ -60,3 +38,56 @@ bdApi(endPoint2)
   .catch(error => {
     console.error('Error:', error);
   });
+
+const test = document.getElementById("division")
+
+test.onclick = function () {
+  dist = document.getElementById("division").value
+
+  const districtsList = document.querySelector('.districts');
+  const upazilaList = document.querySelector('.upazilla');
+  const endPoint = `https://bdapis.com/api/v1.1/division/${dist}`;
+
+  async function bdApi(url) {
+    const res = await fetch(url)
+    return res.json();
+  }
+
+  bdApi(endPoint)
+    .then(districts => {
+      const allDistricts = districts.data;
+
+      allDistricts.forEach(district => {
+        const li = document.createElement("option");
+        li.textContent = district.district;
+        const att = document.createAttribute("value");
+        att.value = district.district;
+        districtsList.append(li);
+
+      })
+      document.querySelector('.districts').onclick = function () {
+        dindex = document.querySelector('.districts').value
+
+        const allUpazilla = districts.data;
+        allUpazilla.forEach(upazilla => {
+          if (dindex == upazilla.district) {
+
+            upa = upazilla.upazilla;
+            for (let i = 0; i < upa.length; i++) {
+              const li = document.createElement("option");
+              li.textContent = upa[i];
+
+              const att = document.createAttribute("value");
+              att.value = upa[i];
+
+              upazilaList.append(li);
+            }
+          }
+        })
+      }
+    })
+    .catch(error => {
+      console.error('Error::', error);
+    });
+}
+
